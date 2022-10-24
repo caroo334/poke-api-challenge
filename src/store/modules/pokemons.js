2512625145df7ef65d1f2ad37f7fea72beb21daf
biteme <caro@Ata-MBP.fibertel.com.ar>
2022-10-23 22:08:53 -0300

    feat: List pokemon and scrollbar

--- src/store/modules/pokemons.js
+++ src/store/modules/pokemons.js
@@ -1,18 +1,43 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import axios from "axios";
+
+export const getAllPokemons = createAsyncThunk(
+  "pokemons/getAllPokemons",
+  async () => {
+    try {
+      const { data } = await axios.get("https://pokeapi.co/api/v2/pokemon");
+      console.log(data);
+      return data;
+    } catch (error) {
+      throw new error;
+    }
+  }
+);
 
 const pokemonsSlice = createSlice({
   name: "pokemons",
   initialState: {
     allPokemons: [],
+    loading: false,
+    error: null,
   },
-  reducers: {
-    getPokemons: (state, action) => {
-        console.log('Funciono');
-        state.allPokemons = ['ivvy', 'Pikachu']
-    },
+  reducers: {},
+  extraReducers: (builder) => {
+    builder.addCase(getAllPokemons.pending, (state, action) => {
+      state.loading = true;
+    })
+    builder.addCase(getAllPokemons.fulfilled, (state, action) => {
+      state.loading= false
+      state.allPokemons = action.payload;
+    });
+    builder.addCase(getAllPokemons.rejected, (state, action) => {
+      state.error = action.error;
+      state.loading= false
+
+    });
   },
 });
 
-export const { getPokemons } = pokemonsSlice.actions;
+export const {} = pokemonsSlice.actions;
 
 export default pokemonsSlice.reducer;
