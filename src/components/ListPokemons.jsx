3e4e74277071fbe6c0636ee5d013df9f1c0b68f5
biteme <caro@Ata-MBP.fibertel.com.ar>
2022-10-25 01:32:55 -0300

    fix: responsive

--- src/components/ListPokemons.jsx
+++ src/components/ListPokemons.jsx
@@ -1,6 +1,19 @@
-import '../styles/ListPokemons.css';
+import "../styles/ListPokemons.css";
+import { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import {
+  getAllPokemons,
+  getNextPokemonsPage,
+  getPrevPokemonsPage,
+} from "../store/modules/pokemons";
+import { Button } from "./Button";
 
 export const ListPokemons = ({ data }) => {
+  const dispatch = useDispatch();
+  const [inputPokemonsNumber, setInputPokemonsNumber] = useState(20);
+  const nextPage = useSelector((state) => state.pokemons.allPokemons.next);
+  const prevPage = useSelector((state) => state.pokemons.allPokemons.previous);
+
   function findImage(url) {
     let matchit = "" + url.match(/[^v2]([0-9]+)/gi);
     matchit = matchit.substring(1);
@@ -10,15 +23,53 @@ export const ListPokemons = ({ data }) => {
       ".png"
     );
   }
+
+  const handleChangeInputPokemonsNumber = (e) => {
+    e.preventDefault();
+    setInputPokemonsNumber(e.target.value);
+  };
+
+  const handleClickNextPage = (e) => {
+    e.preventDefault();
+    dispatch(getNextPokemonsPage(nextPage));
+  };
+
+  const handleClickPrevPage = (e) => {
+    e.preventDefault();
+    dispatch(getPrevPokemonsPage(prevPage));
+  };
+
+  const handleClick = () => {
+    dispatch(getAllPokemons(inputPokemonsNumber));
+  };
+
   return (
     <div>
-      {/* <h2>Lista de Pokemons</h2> */}
-      <div className="box">
+      <div className="pokemons-bring-number-list-container">
+        <input
+          value={inputPokemonsNumber}
+          onChange={handleChangeInputPokemonsNumber}
+          type="number"
+          placeholder="cantidad de pokemones"
+          className="home-input"
+        />
+        <Button onClick={handleClick}>Bring</Button>
+      </div>
+      <div className="pokemons-pagination">
+        {prevPage ? (
+          <Button onClick={handleClickPrevPage}>Previous</Button>
+        ) : null}
+        {nextPage ? <Button onClick={handleClickNextPage}>next</Button> : null}
+      </div>
+      <div className="box row ">
         {data.map((e, i) => {
           return (
-            <ul key={`${e}_${i}`} className="home-list-name-ul">
-              <li className="home-list-name-li">{e.name}</li>{" "}
-              <img src={findImage(e.url)} alt="" />
+            <ul
+              key={`${e}_${i}`}
+              className="home-list-name-ul col-12 col-sm-6 col-md-3"
+            >
+              <li className="home-list-name-li ">{e.name}</li>{" "}
+              <img src={findImage(e.url)} alt="" className="img-fluid" />
             </ul>
           );
         })}
